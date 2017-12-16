4611a6ff5fd0a72f5559cb803ddfdb09a89b22aa
Jelle Vandebeeck <jelle@fousa.be>
2017-12-16 10:08:04 +0100

    The first awesome commit.

---
+++ Gelukszaaiers/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Gelukszaaiers
+//
+//  Created by Jelle Vandebeeck on 16/12/2017.
+//  Copyright © 2017 Gelukszaaiers. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
