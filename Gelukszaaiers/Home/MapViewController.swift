d0c2a4c9c43527ba37156db9ce647680a18b5d86
Jelle Vandebeeck <jelle@fousa.be>
2017-12-16 11:50:41 +0100

    Add a simple map.

---
+++ Gelukszaaiers/Home/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  Gelukszaaiers
+//
+//  Created by Jelle Vandebeeck on 16/12/2017.
+//  Copyright © 2017 Gelukszaaiers. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    // MARK: - Outlets
+
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var seedsLabel: UILabel!
+
+}
