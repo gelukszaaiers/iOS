b178f85ffcb44095a188b83999efe64996621701
Jelle Vandebeeck <jelle@fousa.be>
2017-12-16 10:18:00 +0100

    Add serializer integration.

---
+++ Gelukszaaiers/Service/Models/Profile.swift
@@ -0,0 +1,27 @@
+//
+//  Profile.swift
+//  Gelukszaaiers
+//
+//  Created by Jelle Vandebeeck on 16/12/2017.
+//  Copyright © 2017 Gelukszaaiers. All rights reserved.
+//
+
+import Foundation
+
+struct Profile: Codable {
+
+    struct Authentication: Codable {
+        var accessToken: String?
+        var refreshToken: String?
+    }
+
+    var id: String?
+    var name: String?
+    var email: String?
+    var mobile: String?
+    var verified: Bool?
+    var avatar: URL?
+    var langcode: URL?
+    var authentication: Authentication?
+
+}
---
+++ Gelukszaaiers/Service/Serializer/BackendError.swift
@@ -0,0 +1,17 @@
+//
+//  BackendError.swift
+//  Gelukszaaiers
+//
+//  Created by Jelle Vandebeeck on 16/12/2017.
+//  Copyright © 2017 Gelukszaaiers. All rights reserved.
+//
+
+import Foundation
+
+struct BackendError: LocalizedError {
+
+    var localizedDescription: String {
+        return "Los error"
+    }
+    
+}
