5de62c3bfbdf2b05bfe31f1297df6d9d41e15ee0
Dylan Gyesbreghs <dgyesbreghs@gmail.com>
2017-12-16 12:40:22 +0100

    Add some new files

---
+++ Gelukszaaiers/ApplicationKeys/ApplicationKeys.swift
@@ -0,0 +1,9 @@
+//
+//  ApplicationKeys.swift
+//  Gelukszaaiers
+//
+//  Created by Dylan Gyesbreghs on 16/12/2017.
+//  Copyright © 2017 Gelukszaaiers. All rights reserved.
+//
+
+import Foundation
