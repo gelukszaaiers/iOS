7dee5650c18ee4a4a4fb95eb9e3688c358a6aa28
Dylan Gyesbreghs <dgyesbreghs@gmail.com>
2017-12-16 11:05:18 +0100

    Implement the UIColor+Palette extension

--- Gelukszaaiers/Extensions/UIColor+Palette.swift
+++ Gelukszaaiers/Extensions/UIColor+Palette.swift
@@ -6,4 +6,14 @@
 //  Copyright © 2017 Gelukszaaiers. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    class var lightGold: UIColor {
+        return UIColor(red: 255.0/255.0, green: 210.0/255.0, blue: 69.0/255.0, alpha: 1)
+    }
+    
+    class var mango: UIColor {
+        return UIColor(red: 254.0/255.0, green: 166.0/255.0, blue: 33.0/255.0, alpha: 1)
+    }
+}
