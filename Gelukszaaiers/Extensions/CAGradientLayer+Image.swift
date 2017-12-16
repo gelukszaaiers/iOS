d8428c5532beab7802105c20a9445feadbd47263
Dylan Gyesbreghs <dgyesbreghs@gmail.com>
2017-12-16 11:05:34 +0100

    Create layers

--- Gelukszaaiers/Extensions/CAGradientLayer+Image.swift
+++ Gelukszaaiers/Extensions/CAGradientLayer+Image.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2017 Gelukszaaiers. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension CAGradientLayer {
+    
+    convenience init(frame: CGRect, colors: [UIColor]) {
+        self.init()
+        self.frame = frame
+        self.colors = []
+        for color in colors {
+            self.colors?.append(color.cgColor)
+        }
+        startPoint = CGPoint(x: 0, y: 0)
+        endPoint = CGPoint(x: 0, y: 1)
+    }
+    
+    var gradientImage: UIImage? {
+        var image: UIImage?
+        UIGraphicsBeginImageContext(bounds.size)
+        if let context = UIGraphicsGetCurrentContext() {
+            render(in: context)
+            image = UIGraphicsGetImageFromCurrentImageContext()
+        }
+        UIGraphicsEndImageContext()
+        return image
+    }   
+}
--- Gelukszaaiers/Extensions/UINavigationBar+GradientLayer.swift
+++ Gelukszaaiers/Extensions/UINavigationBar+GradientLayer.swift
@@ -6,4 +6,15 @@
 //  Copyright © 2017 Gelukszaaiers. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UINavigationBar {
+    
+    func setGradientBackground(colors: [UIColor]) {
+        var updatedFrame = bounds
+        updatedFrame.size.height += 20
+        let gradientLayer = CAGradientLayer(frame: updatedFrame, colors: colors)
+        
+        setBackgroundImage(gradientLayer.gradientImage, for: UIBarMetrics.default)
+    }
+}
